d123462900f6a25c0290d07c4c93afdb7220bf46
Admin_2 <alexdii4000@gmail.com>
2021-06-25 08:11:44 +0300

    Initial commit. Completed first version.

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
+        System.out.println("Input DATA below!");
 
         Scanner in = new Scanner(System.in);
         System.out.print("Input name: ");
