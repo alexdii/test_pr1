32490c561ac61af4aca5a0838f242fe0317ef612
Alex Dolzhenko <alexdii4000@gmail.com>
2021-06-26 15:53:35 +0300

    file

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -1,21 +1,13 @@
 package com.company;
-import java.util.Scanner;
-
 
 public class Program {
 
-    public static void main(String[] args) {
-        System.out.println("Input DATA below!");
+    public static void main (String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input name: ");
-        String name = in.nextLine();
-        System.out.print("Input age: ");
-        int age = in.nextInt();
-        System.out.print("Input height: ");
-        float height = in.nextFloat();
-        System.out.printf("Name: %s  Age: %d  Height: %.2f \n", name, age, height);
-        in.close();
+        System.out.print("Hello, Java!");
+        
+
+
     }
 
 }
