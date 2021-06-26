fab9048918201ecd948740eceef7925b9ce2f081
Alex Dolzhenko <alexdii4000@gmail.com>
2021-06-26 15:52:27 +0300

    file

---
+++ src/JFraim/dick.java
@@ -0,0 +1,17 @@
+package JFraim;
+
+import javax.swing.*;
+
+public class dick {
+
+    public static void main(String[] args) {
+        JFrame r = new JFrame(" G-gurda");
+         r.setVisible(true);
+         r.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+         r.setSize(300, 200);
+         r.setResizable( false);
+         r.setLocationRelativeTo(null);
+
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello, Alex_Dii");
-
+        //write your code here
+       System.out.println("Hello, Alex_Dii");
     }
 }
